4286c5710aae31997ad5691223e821f5447cb6e8
Charan Gautam <gautamsaicharan@yahoo.com>
2021-12-05 23:28:44 -0700

    cors routes

--- index.js
+++ index.js
@@ -16,8 +16,19 @@ const app = express();
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // middleware
+let allowedOrigins = ['http://localhost:8080', 'https://avengers-database.herokuapp.com', 'http://localhost:1234', 'https://avengersflix.netlify.app'];
 
-app.use(cors());
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 app.use(morgan('common'));
 app.use(express.static('public'));
 app.use(express.json());
@@ -33,7 +44,7 @@ require('./passport');
 app.post('/movies', (req, res) => {
     Movies.create(req.body)
         .then(movie => {
-            res..status(200).json(movie)
+            res.status(200).json(movie)
         })
         .catch(err => {
             console.error(err);
