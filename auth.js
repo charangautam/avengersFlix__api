f600249057088de749d2f250747784dbe622640b
Charan Gautam <gautamsaicharan@yahoo.com>
2022-01-14 22:07:09 -0700

    jsdoc added to API

--- auth.js
+++ auth.js
@@ -14,23 +14,28 @@ let generateJWTToken = (user) => {
     });
 }
 
-// post login
+
+/**
+ * POST login.
+ * @param {*} router
+ * @returns {object} Token, User
+*/
 module.exports = (router) => {
     router.post('/login', (req, res) => {
-      passport.authenticate('local', { session: false }, (error, user, info) => {
-        if (error || !user) {
-            return res.status(400).json({
-                message: 'Something is not right',
-                user: user
-            });
-        }
-        req.login(user, { session: false }, (error) => {
-            if (error) {
-                res.send(error);
+        passport.authenticate('local', { session: false }, (error, user, info) => {
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'Something is not right',
+                    user: user
+                });
             }
-            let token = generateJWTToken(user.toJSON());
-            return res.json({ user, token });
-        });
-      })(req, res);
+            req.login(user, { session: false }, (error) => {
+                if (error) {
+                    res.send(error);
+                }
+                let token = generateJWTToken(user.toJSON());
+                return res.json({ user, token });
+            });
+        })(req, res);
     });
 }
